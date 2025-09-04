10278be883bc1684a07ed20854ff50c8738a44a2
vantascure <118640518+vantascure@users.noreply.github.com>
2025-09-04 22:26:40 +0530

    feat: replace console outputs with window alerts

--- script.js
+++ script.js
@@ -36,24 +36,23 @@ function playGame() {
 
     // Function for one game round
     function playRound(humanChoice, computerChoice) {
-        console.log(`Player choice is ${humanChoice}.`);
-        console.log(`Computer choice is ${computerChoice}.`);
+        alert(`Player chose ${humanChoice}.\nComputer chose ${computerChoice}.`);
 
         if (humanChoice === computerChoice) {
-            console.log(`It's a tie! Both chose ${humanChoice}.`);
+            alert(`It's a tie! Both chose ${humanChoice}.`);
         } else if (OUTCOMES[humanChoice] === computerChoice) {
             humanScore += 1;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+            alert(`You win! ${humanChoice} beats ${computerChoice}.`);
         } else {
             computerScore += 1;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+            alert(`You lose! ${computerChoice} beats ${humanChoice}.`);
         }
 
-        return `Your score is ${humanScore} | Computer score is ${computerScore}`
+        alert(`Your score is ${humanScore} | Computer score is ${computerScore}`)
     }
 
     while (round < 5) {
-        console.log(playRound(getHumanChoice(), getComputerChoice()));
+        playRound(getHumanChoice(), getComputerChoice());
         round += 1;
     }
 }
